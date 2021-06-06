7fea32004e454f9544e9027fae4243c0e3b6f103
moonjuice <moonjuice1@daum.net>
2021-06-06 03:35:40 -0700

    categories

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,17 +1,31 @@
 import { SetMetadata } from '@nestjs/common';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { Role } from 'src/auth/role.decorator';
 import { User, UserRole } from 'src/users/entities/user.entity';
+import { AllCategoriesOutput } from './dtos/all-categories.dto';
+import { CategoryInput, CategoryOutput } from './dtos/category.dto';
 import {
   CreateRestaurantInput,
   CreateRestaurantOutput,
 } from './dtos/create-restaurant.dto';
-import { DeleteRestaurantInput, DeleteRestaurantOutput } from './dtos/delete-restaurant.dto';
+import {
+  DeleteRestaurantInput,
+  DeleteRestaurantOutput,
+} from './dtos/delete-restaurant.dto';
 import {
   EditRestaurantInput,
   EditRestaurantOutput,
 } from './dtos/edit-restaurant.dto';
+import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
@@ -20,12 +34,12 @@ export class RestaurantResolver {
   constructor(private readonly restaurantService: RestaurantService) {}
 
   @Mutation(returns => CreateRestaurantOutput)
-  @Role(['Any'])
+  @Role(['Owner'])
   async createRestaurant(
     @AuthUser() authUser: User,
     @Args('input') createRestaurantInput: CreateRestaurantInput,
   ): Promise<CreateRestaurantOutput> {
-    console.log("CHECK THIS!!!", authUser)
+    console.log("AUTH USR!", authUser)
     return this.restaurantService.createRestaurant(
       authUser,
       createRestaurantInput,
@@ -47,6 +61,29 @@ export class RestaurantResolver {
     @AuthUser() owner: User,
     @Args('input') deleteRestaurantInput: DeleteRestaurantInput,
   ): Promise<DeleteRestaurantOutput> {
-    return this.restaurantService.editRestaurant(owner, deleteRestaurantInput);
+    return this.restaurantService.deleteRestaurant(
+      owner,
+      deleteRestaurantInput,
+    );
+  }
+}
+
+@Resolver(of => Category)
+export class CategoryResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
+  @ResolveField(type => Int)
+  restaurantCount(@Parent() category: Category): Promise<number> {
+    return this.restaurantService.countRestaurants(category);
+  }
+
+  @Query(type => AllCategoriesOutput)
+  allCategories(): Promise<AllCategoriesOutput> {
+    return this.restaurantService.allCategories();
+  }
+
+  @Query(type => CategoryOutput)
+  category(@Args() categoryInput: CategoryInput): Promise<CategoryOutput> {
+    return this.restaurantService.findCategoryBySlug(categoryInput);
   }
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { AllCategoriesOutput } from './dtos/all-categories.dto';
+import { CategoryInput } from './dtos/category.dto';
 import {
   CreateRestaurantInput,
   CreateRestaurantOutput,
@@ -30,7 +32,6 @@ export class RestaurantService {
     try {
       const newRestaurant = this.restaurants.create(createRestaurantInput);
       newRestaurant.owner = owner;
-      console.log(newRestaurant)
       const category = await this.categories.getOrCreate(
         createRestaurantInput.categoryName,
       );
@@ -58,14 +59,14 @@ export class RestaurantService {
       if(!restaurant){
         return{
           ok: false,
-          error: 'Restaurant not ofund'
+          error: 'Restaurant not found'
         }
       }
       
       if(owner.id !== restaurant.ownerId){
         return {
           ok:false, 
-          error: "YOu can't edit a restaurant you don't own"
+          error: "You can't edit a restaurant you don't own"
         }
       }
       let category: Category = null;
@@ -116,5 +117,48 @@ export class RestaurantService {
         error: 'Could not delete restaurant.',
       };
     }
+  }
+
+  async allCategories(): Promise<AllCategoriesOutput>{
+
+    try{
+      const categories = await this.categories.find();
+      return{
+        ok: true,
+        categories
+      }
+    }catch{
+      return {
+        ok:false,
+        error: "Could not load categories"
+      }
+    }
+
+  }
+
+  countRestaurants(category: Category) {
+    return this.restaurants.count({ category });
+  }
+
+
+  async findCategoryBySlug({slug}:CategoryInput){
+      try {
+        const category = await this.categories.findOne({ slug }, {relations: ['restaurants']});
+        if(!category){
+          return {
+            ok: false,
+            error: "Category not found"
+          }
+        }
+        return {
+          ok: true,
+          category
+        }
+      } catch {
+        return {
+          ok:false,
+          error: "Could not load category"
+        }
+      }
   }
 }
